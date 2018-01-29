9bfd1f2beceb3838777fd7ca8708c7098cc877f8
Student <Student@VMMB300-VHD-1>
2018-01-29 10:18:58 +0200

    Add project files.

---
+++ FS_Justuus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FS_Justuus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
