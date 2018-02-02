c537a5a8017774c18b64070cb24cd5865a5d6fa4
Student <Student@VMMB300-VHD-1>
2018-02-02 12:45:31 +0200

    WhatHappensWhenItAllEnds

--- FS_Justuus/Program.cs
+++ FS_Justuus/Program.cs
@@ -4,9 +4,66 @@ namespace FS_Justuus
 {
     class Program
     {
+        private static int prevStage;
+
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        {            
+            Start:
+
+            bool Death = false;
+
+            //initialize player stat variables
+            int playerHP = 10;
+            Console.WriteLine("What is your name, adventurer?");
+            string charName = Console.ReadLine();
+
+
+            //randomization code
+
+            //precombat
+            
+            int prevStage = 0;
+
+            int stage = prevStage + 1;
+            prevStage = stage;
+
+
+            //enter combat text
+
+            //combat
+
+            //end of combat story
+
+            //death, if it happens
+                      
+
+            if (playerHP <= 0)
+            {
+                Death = true;
+                if (Death == true)
+                {
+                    Console.WriteLine("You fought well "+ charName + ", but it wasn't enough...");
+                    Console.ReadKey();
+                    Console.WriteLine("Willing to play more?(Y/N)");
+                    string key = Console.ReadLine();
+                    if (key.StartsWith( "y") | key.StartsWith("Y"))
+                    {
+                        goto Start;
+                    }
+                    else
+                    {
+                     
+                    };
+                    
+                };
+            }
+            
+            else
+            {
+               
+            };
+        }        
+            //end texts
+        
     }
 }
