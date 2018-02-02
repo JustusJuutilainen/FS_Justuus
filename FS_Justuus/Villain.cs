6349f8138afbbd82a0c1727b5599623b251c31d6
Student <Student@VMMB300-VHD-1>
2018-02-02 13:16:04 +0200

    Corrections to ids

--- FS_Justuus/Villain.cs
+++ FS_Justuus/Villain.cs
@@ -16,10 +16,10 @@ namespace FS_Justuus
         List<Villain> villainGroup = new List<Villain>()
         {
             new Villain { villainId=1, villainName="Drunkard", Hp=3, Lvl=1},
-            new Villain { villainId=5, villainName="Maniac", Hp=4, Lvl=1},
-            new Villain { villainId=2, villainName="Retired Thief", Hp=5, Lvl=2},
-            new Villain { villainId=3, villainName="Flexing Barbarian", Hp=7, Lvl =3},
-            new Villain { villainId=4, villainName="Twisted Druid", Hp=9, Lvl =4},
+            new Villain { villainId=2, villainName="Maniac", Hp=4, Lvl=1},
+            new Villain { villainId=3, villainName="Retired Thief", Hp=5, Lvl=2},
+            new Villain { villainId=4, villainName="Flexing Barbarian", Hp=7, Lvl =3},
+            new Villain { villainId=5, villainName="Twisted Druid", Hp=9, Lvl =4},
             
         };
     }
