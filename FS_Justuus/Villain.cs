0b83021cfca74076e572d33b37540e6685124652
Student <Student@VMMB300-VHD-1>
2018-02-02 12:52:50 +0200

    weaponry/villains

---
+++ FS_Justuus/Villain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FS_Justuus
+{
+    public class Villain
+    {
+        //Properties
+        public string villainName { get; set; }
+        public int Hp { get; set; }
+        public int Lvl { get; set; }
+               
+        List<Villain> villainGroup = new List<Villain>()
+        {
+            new Villain { villainName="Clumsy, almost retired Thief", Hp=3, Lvl=1},
+            new Villain { villainName="The Flexing Barbarian", Hp=5, Lvl =2},
+            new Villain { villainName="The Twisted Druid", Hp=7, Lvl =3},
+
+        };
+    }
+}
---
+++ FS_Justuus/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FS_Justuus
+{
+    class Weapon
+    {
+        public string weaponName { get; set; }
+        public int damage { get; set; }
+
+        List<Weapon> weaponry = new List<Weapon>()
+        {
+
+        }
+    }
+}
+}
