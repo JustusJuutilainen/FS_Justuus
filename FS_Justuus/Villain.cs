2c45b3063bff92093bdae7bb019f8fb019ad4255
Student <Student@VMMB300-VHD-1>
2018-02-02 13:15:10 +0200

    AdditionsToLists

--- FS_Justuus/Villain.cs
+++ FS_Justuus/Villain.cs
@@ -7,16 +7,20 @@ namespace FS_Justuus
     public class Villain
     {
         //Properties
+        public int villainId { get; set; }
         public string villainName { get; set; }
         public int Hp { get; set; }
         public int Lvl { get; set; }
+        
                
         List<Villain> villainGroup = new List<Villain>()
         {
-            new Villain { villainName="Clumsy, almost retired Thief", Hp=3, Lvl=1},
-            new Villain { villainName="The Flexing Barbarian", Hp=5, Lvl =2},
-            new Villain { villainName="The Twisted Druid", Hp=7, Lvl =3},
-
+            new Villain { villainId=1, villainName="Drunkard", Hp=3, Lvl=1},
+            new Villain { villainId=5, villainName="Maniac", Hp=4, Lvl=1},
+            new Villain { villainId=2, villainName="Retired Thief", Hp=5, Lvl=2},
+            new Villain { villainId=3, villainName="Flexing Barbarian", Hp=7, Lvl =3},
+            new Villain { villainId=4, villainName="Twisted Druid", Hp=9, Lvl =4},
+            
         };
     }
 }
--- FS_Justuus/Weapon.cs
+++ FS_Justuus/Weapon.cs
@@ -6,13 +6,20 @@ namespace FS_Justuus
 {
     class Weapon
     {
+        public int weaponId { get; set; }
         public string weaponName { get; set; }
-        public int damage { get; set; }
+        public int damageBonus { get; set; }
 
         List<Weapon> weaponry = new List<Weapon>()
         {
+            new Weapon { weaponId=1, weaponName="Rusty Knife", damageBonus = 1},
+            new Weapon { weaponId=2, weaponName="Wooden Staff", damageBonus=1},
+            new Weapon { weaponId=3, weaponName="Dagger", damageBonus = 2},
+            new Weapon { weaponId=4, weaponName="Whip", damageBonus=2},
+            new Weapon { weaponId=5, weaponName="Axe", damageBonus = 3},
+            new Weapon { weaponId=6, weaponName="Silver Sword", damageBonus = 3},                      
 
-        }
+        };
     }
 }
-}
+
