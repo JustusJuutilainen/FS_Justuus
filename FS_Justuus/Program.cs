3d97e1a6c137673520e83a8f1383ca3aa42903a5
Student <Student@VMMB300-VHD-1>
2018-02-12 11:27:22 +0200

    Villain and Weapon classes complete

--- FS_Justuus/Program.cs
+++ FS_Justuus/Program.cs
@@ -11,33 +11,57 @@ namespace FS_Justuus
     {
         //private static int prevStage;
 
-           
+
         static void Main(string[] args)
         {
-            Start:        
-            
-            bool Death = false;
 
+            //start music
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "\\Tribal Ritual.wav";
+            player.PlayLooping();
+
+            Start:
             //initialize player stat variables
+            int postCombatHealing = 2;
             int playerHP = 10;
+            int charBaseDamage = 2;
             Console.WriteLine("What is your name, adventurer?");
             string charName = Console.ReadLine();
 
-
+            int prevStage = 0;
             //randomization code
 
-            //precombat
+            //intro
+            Console.WriteLine("\nYou, " + charName + ", are a mercenary warrior from the Kingdom of Saaria.");
+            Console.WriteLine("The king Jan II has hired you to assasinate a rogue Jarl.");
+            Console.WriteLine("In order to get to the Jarl, you must first dispatch of his assortment of bodyguards.");
+            Console.WriteLine("You start with the first bodyguard, who you challenge to combat.\n");
+            Console.ReadLine();
 
-            int prevStage = 0;
+
+
+            //precombat
+            Precombat:
 
             int stage = prevStage + 1;
             prevStage = stage;
-
+            string villainName = "Test Monster";
+            int villainHP = 4;
+            int villainBaseDamage = 10;
 
             //enter combat text
 
-            //combat
+            Console.WriteLine("You have encountered a wild " + villainName);
+            Console.ReadLine();
+
+            //WpnRandomization
+            //To use weapon call the variables wpnUsed & wpnAB
+            //remember to include also the wpn randomizer to the loop 
+            //so it won't use just one weapon with all the villains
+
             AllWeapons weapon = new AllWeapons();
+            string wpnUsed = "None";
+            int wpnAB = 2;
 
             Random rnd = new Random();
             int rndWpn = rnd.Next(0, 15);
@@ -46,41 +70,101 @@ namespace FS_Justuus
                               where wpn.weaponId == rndWpn
                               select wpn)
             {
-                Console.WriteLine("Weapon name: " + w.weaponName + ", Damage bonus: " + w.damageBonus);
-            }
+                wpnUsed = w.weaponName;
+                wpnAB = w.damageBonus;
+            }            
 
-            //end of combat story
+            //Villain system for combat, (villainId = current stage)
+            AllVillains villain = new AllVillains();
+            string crntVillain = "Test Monster";    
+            int crntVillainHp = 1;
+            int villainAB = 1;
+            int villainBD = 1;
 
-            //death, if it happens
-
-
-            if (playerHP <= 0)
+                foreach (var v in from vill in villain
+                                  where vill.villainId == stage
+                                  select vill)
                 {
-                    Death = true;
-                    if (Death == true)
-                    {
-                        Console.WriteLine("You fought well "+ charName + ", but it wasn't enough...");
-                        Console.ReadKey();
-                        Console.WriteLine("Willing to play more?(Y/N)");
-                        string key = Console.ReadLine();
-                        if (key.StartsWith( "y") | key.StartsWith("Y"))
-                        {
-                            goto Start;
-                        }
-                        else
-                        {
+                    crntVillain = v.villainName;
+                    crntVillainHp = v.villainHp;
+                    villainAB = v.villainAttackBonus;
+                    villainBD = v.villainBaseDamage;
+                }                    
+                            
 
-                        };
+            ////combat            
 
-                    };
+            while (playerHP > 0)
+            {
+                Console.WriteLine("You hit " + villainName + " for " + charBaseDamage + " damage.");
+                villainHP = villainHP - charBaseDamage;
+                Console.WriteLine(villainName + " has " + villainHP + " HP left.");
+                Console.ReadLine();
+                if (villainHP < 1)
+                {
+                    break;
                 }
 
+                Console.WriteLine(villainName + " attacks you for " + villainBaseDamage + " damage.");
+                playerHP = playerHP - villainBaseDamage;
+                Console.WriteLine(charName + " has " + playerHP + " HP left.");
+                Console.ReadLine();
+
+            }
+
+            //death, if it happens
+            if (playerHP < 1)
+            {
+                Console.WriteLine("\nYou have died. The Jarl no longer won't have his plans ruined.\n");
+                Console.ReadKey();
+                Console.WriteLine("Willing to play more?(Y/N)");
+                string key = Console.ReadLine();
+                if (key.StartsWith("y") | key.StartsWith("Y"))
+                {
+                    goto Start;
+                }
                 else
                 {
+                    goto End;
+                }
+            };
+            //end of combat story
 
-                };
-            }        
+
+            Console.WriteLine("\nYou have completed level " + stage);
+            playerHP = playerHP + 2;
+            Console.WriteLine("You gain " + postCombatHealing + " HP.\n");
+
+            //nextstage
+            if (stage < 10)
+            {
+                goto Precombat;
+            }
+
+
+
+
 
             //end texts
-        }       
+            Console.ReadLine();
+            if (String.Equals(charName, "Timo"))
+            {
+                Console.WriteLine("After you have defeated the Jarl, you grab his necklace to bring to the king.");
+                Console.WriteLine("After you present the necklace to the king, he shouts from his throne.");
+                Console.WriteLine("'CONGRATULATIONS! YOU HAVE EARNED A COFFEE BREAK!'");
+                Console.WriteLine("For this reason, the king lets you drink coffee publicly.");
+            }
+            else
+            {
+                Console.WriteLine("After you have defeated the Jarl, you grab his necklace to bring to the king.");
+                Console.WriteLine("After you present the necklace to the king, he shouts from his throne.");
+                Console.WriteLine("'THAT WAS THE WRONG JARL. THIS ONE WAS MY MOST LOYAL, YOU MONGREL!'");
+                Console.WriteLine("For this treason, the king has you executed publicly.");
+            }
+            End:;
+
+            //https://github.com/otuju004/
+            //https://github.com/JustusJuutilainen/FS_Justuus
+        }
     }
+}
--- FS_Justuus/Villain.cs
+++ FS_Justuus/Villain.cs
@@ -4,27 +4,27 @@ using System.Text;
 
 namespace FS_Justuus
 {
-    public class Villain
+    class Villain
     {
-        //Properties
+
         public int villainId { get; set; }
+
         public string villainName { get; set; }
+
         public int villainHp { get; set; }
-        public int villainLvl { get; set; }
-        
-               
-        List<Villain> villainGroup = new List<Villain>()
+
+        public int villainBaseDamage { get; set; }
+
+        public int villainAttackBonus { get; set; }
+
+
+        public Villain(int nextVillainId, string nextVillainName, int nextVillainHP, int nextVillainBaseDamage, int nextvillainAttackBonus)
         {
-            new Villain { villainId=1, villainName="Drunkard", villainHp=3, villainLvl=1},
-            new Villain { villainId=2, villainName="Maniac", villainHp=4, villainLvl=1},
-            new Villain { villainId=3, villainName="Retired Thief", villainHp=5, villainLvl=2},
-            new Villain { villainId=4, villainName="Cursed Kid", villainHp=6, villainLvl =2},            
-            new Villain { villainId=5, villainName="Irritated Pirate", villainHp=7, villainLvl =3},
-            new Villain { villainId=6, villainName="Flexing Barbarian", villainHp=8, villainLvl =3},            
-            new Villain { villainId=7, villainName="Twisted Druid", villainHp=9, villainLvl =4},
-            new Villain { villainId=8, villainName="Injured Berserker", villainHp=10, villainLvl =4},            
-            new Villain { villainId=9, villainName="Psychotic Priest", villainHp=12, villainLvl =5},
-            new Villain { villainId=10, villainName="The Jarl", villainHp=15, villainLvl =6},
-        };                        
+            villainId = nextVillainId;
+            villainName = nextVillainName;
+            villainHp = nextVillainHP;
+            villainBaseDamage = nextVillainBaseDamage;
+            villainAttackBonus = nextvillainAttackBonus;
+        }
     }
 }
